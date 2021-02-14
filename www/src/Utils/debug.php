d3e7994062d4ce8daf5f977ed325c91cb9aeb0a0
kry.ign93 <kris_ignatiuk@wp.pl>
2021-02-14 20:48:16 +0100

    refactor

---
+++ www/src/Utils/debug.php
@@ -0,0 +1,16 @@
+<?php
+
+function dumb($data)
+{
+    echo '<div 
+    style="
+    display: inline-block;
+    padding: 10px;
+    background: lightgray">
+    
+<pre>';
+    print_r($data);
+    echo '</pre>
+</div>
+</br>';
+}
