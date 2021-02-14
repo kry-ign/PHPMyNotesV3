90173152a0287fbe9fbfc952ca961714cc02ee36
kry.ign93 <kris_ignatiuk@wp.pl>
2021-02-14 20:36:33 +0100

    function dumb

--- www/index.php
+++ www/index.php
@@ -1,19 +1,20 @@
 <?php
-// phpinfo();
 
-$link = mysqli_connect("mysql", "root", "password", null);
+$name = 'test dumb';
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+
+function dumb($data)
+{
+    echo '<div 
+    style="
+    display: inline-block;
+    padding: 10px;
+    background: lightgray">
+    
+<pre>';
+    print_r($data);
+    echo '</pre>
+</div>';
 }
 
-echo "Success: A proper connection to MySQL was made!" . PHP_EOL. "<br/>";
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL. "<br/>";
-echo "MySQL Server version: ".$link->server_version;
-
-mysqli_close($link);
-
-?>
+dumb($name);
