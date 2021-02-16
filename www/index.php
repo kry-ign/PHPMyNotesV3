3118d64b8b15eb01fd83cf00b87f9363423d602e
kry.ign93 <kris_ignatiuk@wp.pl>
2021-02-16 16:56:15 +0100

    reafctoring code with class controller.php

--- www/index.php
+++ www/index.php
@@ -4,42 +4,12 @@ declare(strict_types=1);
 
 namespace App;
 
-require ("src/Utils/debug.php");
-require ("src/View.php");
+require_once ("src/Utils/debug.php");
+require_once ("src/Controller.php");
 
-const DEFAULT_ACTION = 'list';
-$action = $_GET['action'] ?? DEFAULT_ACTION;
+$controller = new Controller($_GET, $_POST);
+$controller->run();
 
-$view = new View();
-
-$viewParams = [];
-
-switch ($action) {
-    case 'create':
-        $page = 'create';
-        $created = false;
-        if (!empty($_POST)) {
-            $created = true;
-            $viewParams = [
-                'title' => $_POST['title'],
-                'description' => $_POST['description']
-            ];
-        }
-        $viewParams['created'] = $created;
-    break;
-    case 'show' :
-        $viewParams = [
-            'title' => 'Moja notatka',
-            'descriptions' => 'Opis'
-        ];
-    break;
-    default:
-        $page = 'list';
-        $viewParams['resultList'] = "wyswietlamy notatki";
-    break;
-}
-
-$view->render($page, $viewParams);
 
 
 
